830d09f079120bda41b5f4271b9dd8352a322da9
Stijn Bollen <71003327+s-bollen-cbs@users.noreply.github.com>
2020-09-17 17:48:06 +0200

    update

--- Demo/src/app/app.component.ts
+++ Demo/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<bl5-core></bl5-core>',
-  styleUrls: ['./app.component.scss']
+  template: '<bl5-core></bl5-core>'
 })
 export class AppComponent { }
